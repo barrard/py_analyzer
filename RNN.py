27e61debe3e67042ae01077a78bf6f1aeffaae43
Dave <barrard@gmail.com>
2019-08-28 12:37:32 -1000

    first commit

--- RNN.py
+++ RNN.py
@@ -4,6 +4,18 @@ import pandas as pd
 
 import db
 
+
+SEQ_LEN = 60
+FUTURE_PREIOD_PREDICT = 3
+
+COMM_TO_PREDICT = 'ES'
+
+def classify(current, future):
+  if float(future) > float(current):
+    return 1
+  else:
+    return 0
+
 sym_array = ['/ES', '/CL', '/GC']
 main_df = pd.DataFrame()
 """ request data from DB """
